92dc33a834880a35db34e2292308cc774d1372e0
williamhaw <me@williamhaw.com>
2026-02-24 11:34:49 -0800

    Add utils.Connector

---
+++ internal/utils/connector.go
@@ -0,0 +1,85 @@
+package utils
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/duckdb/duckdb-go/v2"
+)
+
+type Connector struct {
+	connector *duckdb.Connector
+	db        *sql.DB
+}
+
+func NewConnector(dsn string) (*Connector, error) {
+	params := parseDSN(dsn)
+	tokens, hasToken := params["motherduck_token"]
+	if !hasToken {
+		db, err := sql.Open("duckdb", dsn)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open duckdb: %w", err)
+		}
+		return &Connector{db: db}, nil
+	} else {
+		params.Del("motherduck_token")
+	}
+
+	c, err := duckdb.NewConnector(fmt.Sprintf(":memory:?%s", params.Encode()), func(execer driver.ExecerContext) error {
+		bootQueries := []string{
+			`INSTALL motherduck`,
+			`LOAD motherduck`,
+			fmt.Sprintf("SET motherduck_token='%s'", tokens[0]),
+			`ATTACH 'md:'`,
+		}
+		for _, query := range bootQueries {
+			_, err := execer.ExecContext(context.Background(), query, nil)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create connector: %w", err)
+	}
+	return &Connector{connector: c, db: sql.OpenDB(c)}, nil
+}
+
+func parseDSN(dsn string) url.Values {
+	empty := url.Values{}
+	parts := strings.SplitN(dsn, "?", 2)
+	if len(parts) != 2 {
+		return empty
+	}
+
+	params, err := url.ParseQuery(parts[1])
+	if err != nil {
+		return empty
+	}
+	return params
+}
+
+func (c *Connector) GetDB() *sql.DB {
+	return c.db
+}
+
+func (c *Connector) Close() error {
+	if c.db != nil {
+		err := c.db.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close database: %w", err)
+		}
+	}
+	if c.connector != nil {
+		err := c.connector.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close connector: %w", err)
+		}
+	}
+	return nil
+}
